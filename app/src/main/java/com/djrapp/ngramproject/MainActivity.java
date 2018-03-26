8a2baffc66542e434250871f52aaf60eb082fcde
James Weng <wengj1@bxscience.edu>
2018-03-26 09:56:13 -0400

    First commit

---
+++ app/src/main/java/com/djrapp/ngramproject/MainActivity.java
@@ -0,0 +1,13 @@
+package com.djrapp.ngramproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
