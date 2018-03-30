d808c82fc2f8ac7d941b7b5e058dfbe14bd00dd1
Derek Qu <qud@bxscience.edu>
2018-03-30 13:59:32 -0400

    Add Text to Speech.

--- app/src/main/java/com/djrapp/ngramproject/MainActivity.java
+++ app/src/main/java/com/djrapp/ngramproject/MainActivity.java
@@ -1,12 +1,26 @@
 package com.djrapp.ngramproject;
 
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
 
-public class MainActivity extends AppCompatActivity {
+
+import org.w3c.dom.Text;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener{
     private FirebaseAnalytics mFirebaseAnalytics;
+    private TextView lyrics;
+    private Button generateBT, textToSpeechBT;
+    private TextToSpeech tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +28,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        lyrics = findViewById(R.id.lyrics);
+        generateBT = findViewById(R.id.generate);
+        textToSpeechBT = findViewById(R.id.toSpeech);
+
+        tts = new TextToSpeech(MainActivity.this, MainActivity.this);
+        generateBT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        textToSpeechBT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextToSpeechFunction();
+            }
+        });
+    }
+
+    public void TextToSpeechFunction(){
+        String text = lyrics.getText().toString();
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    @Override
+    public void onDestroy(){
+        tts.shutdown();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onInit(int status){
+        if(status == TextToSpeech.SUCCESS){
+            tts.setLanguage(Locale.UK);
+        }
     }
 }
