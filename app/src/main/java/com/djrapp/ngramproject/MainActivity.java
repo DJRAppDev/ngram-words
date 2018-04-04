e1aaab5f8d3ec4f28baf3ad910128d89fc421dec
Derek Qu <qud@bxscience.edu>
2018-04-03 20:07:23 -0400

    Add three arraylists with the different fields.

--- app/src/main/java/com/djrapp/ngramproject/MainActivity.java
+++ app/src/main/java/com/djrapp/ngramproject/MainActivity.java
@@ -17,6 +17,9 @@ import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Text;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener{
@@ -24,6 +27,7 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
     private TextView lyrics;
     private Button generateBT, textToSpeechBT;
     private TextToSpeech tts;
+    private ArrayList<String> lyricList, frequencyList, percentageList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +38,10 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         lyrics = findViewById(R.id.lyrics);
         generateBT = findViewById(R.id.generate);
         textToSpeechBT = findViewById(R.id.toSpeech);
+
+        lyricList = new ArrayList<>();
+        frequencyList = new ArrayList<>();
+        percentageList = new ArrayList<>();
 
         tts = new TextToSpeech(MainActivity.this, MainActivity.this);
         generateBT.setOnClickListener(new View.OnClickListener() {
@@ -48,11 +56,20 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
                 TextToSpeechFunction();
             }
         });
-
-        database.addListenerForSingleValueEvent(new ValueEventListener() {
+        database.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                Log.d("Snapshot",dataSnapshot.getValue().toString());
+                DataSnapshot tempSnapShot = dataSnapshot.child("0").child("data");
+                for(DataSnapshot snapshots : tempSnapShot.getChildren()){
+                    String lyricLine = (String) snapshots.child("Field1").getValue();
+                    String frequency = snapshots.child("Field3").getValue() + "";
+                    String percentage = snapshots.child("Field5").getValue() + "";
+
+                    lyricList.add(lyricLine);
+                    frequencyList.add(frequency);
+                    percentageList.add(percentage);
+                }
+                //Run methods after the for loop
             }
 
             @Override
@@ -77,6 +94,12 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
     public void onInit(int status){
         if(status == TextToSpeech.SUCCESS){
             tts.setLanguage(Locale.UK);
+        }
+    }
+
+    public void printArrayList(ArrayList<String> arrList){
+        for(int i = 0; i < arrList.size(); i++){
+            Log.d("Abc", arrList.get(i));
         }
     }
 }
